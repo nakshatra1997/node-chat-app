682dc940ce31ed80b8078e8611bedfd0540d539a
nakshtra1997 <nakshatrapradhan2013@gmail.com>
2018-02-13 20:02:22 +0530

    little bit styling

--- public/js/index.js
+++ public/js/index.js
@@ -9,21 +9,39 @@
 	
 	});
 	socket.on('newMessage',function (message){
-		// console.log('your message is',message);
 		var formattedTime=moment(message.createdAt).format('h:mm a');
-		var li=$('<li></li>');
-	    li.text(`${message.from} ${formattedTime}:${message.text}`);
-	    $("#messages").append(li);
+		var template=$('#message-template').html();
+		var html=Mustache.render(template,{
+			text:message.text,
+			from:message.from,
+			createdAt:formattedTime
+		});
+		$('#messages').append(html);
+		// console.log('your message is',message);
+		// var formattedTime=moment(message.createdAt).format('h:mm a');
+		// var li=$('<li></li>');
+	 //    li.text(`${message.from} ${formattedTime}:${message.text}`);
+	 //    $("#messages").append(li);
 	});
 
 	socket.on('newLocationMessage',function(message){
+		// console.log(message);
 		var formattedTime=moment(message.createdAt).format('h:mm a');
-        var li=$('<li></li>');
-        var a=$('<a target="_blank">my current location</a>');
-	    li.text(`${message.from} ${formattedTime}:`);
-	    a.attr('href',message.url);
-	    li.append(a);
-	    $('#messages').append(li);
+		var template=$('#location-message-template').html();
+	
+		
+		var html=Mustache.render(template,{
+			url:message.url,
+			from:message.from,
+			createdAt:formattedTime
+		});
+		$('#messages').append(html);
+     //    var li=$('<li></li>');
+     //    var a=$('<a target="_blank">my current location</a>');
+	    // li.text(`${message.from} ${formattedTime}:`);
+	    // a.attr('href',message.url);
+	    // li.append(a);
+	    // $('#messages').append(li);
 	});
 	socket.on('disconnect',function (){
 		console.log('disconnected from server');
